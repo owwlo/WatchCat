ef1ccc34691697319f26dc4f341bb961718d898a
owwlo <owwlo.g@gmail.com>
2021-12-02 21:34:34 -0800

    fix context in Toaster #hide

--- app/src/main/java/org/owwlo/watchcat/utils/Toaster.java
+++ app/src/main/java/org/owwlo/watchcat/utils/Toaster.java
@@ -1,6 +1,5 @@
 package org.owwlo.watchcat.utils;
 
-import android.content.Context;
 import android.util.Log;
 import android.widget.Toast;
 
@@ -45,29 +44,29 @@ public class Toaster {
         }
     }
 
-    public static void info(Context context, final String msg) {
+    public static void info(final String msg) {
         WatchCatApp.postOnMainThread(new Runnable() {
             @Override
             public void run() {
-                Toasty.info(context, msg, Toast.LENGTH_SHORT, false).show();
+                Toasty.info(WatchCatApp.sContext, msg, Toast.LENGTH_SHORT, false).show();
             }
         });
     }
 
-    public static void warn(Context context, final String msg) {
+    public static void warn(final String msg) {
         WatchCatApp.postOnMainThread(new Runnable() {
             @Override
             public void run() {
-                Toasty.warning(context, msg, Toast.LENGTH_SHORT, false).show();
+                Toasty.warning(WatchCatApp.sContext, msg, Toast.LENGTH_SHORT, false).show();
             }
         });
     }
 
-    public static void error(Context context, final String msg) {
+    public static void error(final String msg) {
         WatchCatApp.postOnMainThread(new Runnable() {
             @Override
             public void run() {
-                Toasty.error(context, msg, Toast.LENGTH_SHORT, false).show();
+                Toasty.error(WatchCatApp.sContext, msg, Toast.LENGTH_SHORT, false).show();
             }
         });
     }
